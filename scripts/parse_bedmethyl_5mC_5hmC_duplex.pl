c2c9425c1180151b70a5988349216f5e5374e2c5
dmsoanes <d.m.soanes@exeter.ac.uk>
2025-06-10 11:00:34 +0100

    Create parse_bedmethyl_5mC_5hmC_duplex.pl

---
+++ scripts/parse_bedmethyl_5mC_5hmC_duplex.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+#open (INPUT, "Sox10_methyl_5mC_5hmC_head_1000.bed");
+#open (OUTPUThm, ">Sox10_methyl_5mC_5hmC_head_1000_hmC_out.bed");
+#open (OUTPUTm, ">Sox10_methyl_5mC_5hmC_head_1000_mC_out.bed");
+
+$num_args=$#ARGV+1;
+if ($num_args !=1) {
+	print "\nUSAGE: parse_bedmethyl_5mC_5hmC_duplex.pl <input.bed>\n";
+	exit;
+}
+$input = "$ARGV[0]";
+open (INPUT,$input);
+$out = substr($input,0,-4)."_out.txt";
+open (OUTPUT, ">$out");
+
+
+print OUTPUT "Chromosome\tStart\tEnd\tModification\tValid_Coverage\tPer_methylation\n";
+while ($text=<INPUT>){
+	chomp($text);
+	@list=split(/\t/,$text);
+	($Chromosome, $Start, $End, $mod, $col5, $col6, $col7, $col8, $col9, $col10)=@list;
+	@list_col10=split(/ /,$col10);
+	($cov,$per)=@list_col10;
+	print OUTPUT "$Chromosome\t$Start\t$End\t$mod\t$cov\t$per\n";
+}
+
+close (INPUT);
+close (OUTPUT);
